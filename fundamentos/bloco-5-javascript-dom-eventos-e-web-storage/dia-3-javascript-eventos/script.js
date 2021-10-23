bef82eac2dca59b4c5b186196d14f30f9149bc9c
AdrianFadiga <adrianmfadiga@gmail.com>
2021-10-23 00:52:18 -0300

    primeira parte resolvida

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,47 @@
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+
+let elementoLi = document.querySelectorAll('li');
+for (let i = 0; i < elementoLi.length; i +=1) {
+  elementoLi[i].addEventListener("click", recebeClick);
+}
+
+function recebeClick (eventoDeOrigem) {
+  for (let i = 0; i <= 3; i++) {
+    if (eventoDeOrigem.target === elementoLi[i]) {
+  elementoLi[i].className = 'tech';
+  }
+}
+}
+
+// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portifólio?
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
